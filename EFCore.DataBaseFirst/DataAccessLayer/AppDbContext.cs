007041067e5289f9104049a094e9718c6adb5c60
alper yazır <alper.yazir@outlook.com>
2022-09-14 00:11:53 +0300

    DbContext classı oluşturuldu.
    appsettings.json dosyası oluşturuldu.
    
    aappsettings dosyasınan okuma yapabilmek için DbContextInitialize classı yazıldı ve Configuration üzerinden okuma sağlandı

---
+++ EFCore.DataBaseFirst/DataAccessLayer/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using EFCore.DataBaseFirst.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.DataBaseFirst.DataAccessLayer
+{
+    public class AppDbContext : DbContext
+    {
+       
+        public DbSet<Products> Products { get; set; }
+
+
+        public AppDbContext()
+        {
+
+        }
+
+        //Dbden okuma 1.Yol
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        //Dbden okuma 2.Yol
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbContextInitializer.Configuration.GetConnectionString("SqlCon"));
+        }
+    }
+}
--- EFCore.DataBaseFirst/Program.cs
+++ EFCore.DataBaseFirst/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCore.DataBaseFirst.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+
+
+//Console uygulaması ayakta olduğu sürece, ilk seferde initialize edilsin ve daha sonrasında işlemleri devam etsin amacında kullanılır
+DbContextInitializer.Build();
+
+
+//using bir işlem dispose olduktan sonra otomatik olarak connection bağlantısının kapatılması ve memory i kastırmaması için kullanılır.
+//Appsettings dosyasındaki SqlConnection ı kullanmak için yazılan classlar
+using (var _context = new AppDbContext(DbContextInitializer.OptionsBuilder.Options))
+{
+    var products = await _context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} : {p.Name}");
+    });
+    
+    Console.ReadKey();
+}
