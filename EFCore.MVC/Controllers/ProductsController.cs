6daf31d7f8975f54bc0d68121f8c4047a9af8cc5
alper yazır <alper.yazir@outlook.com>
2023-05-15 22:59:09 +0300

    update

---
+++ EFCore.MVC/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using EFCore.MVC.DAL;
+using EFCore.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context = null)
+        {
+            _context = context;
+        }
+
+        public async  Task<IActionResult> Index()
+        {
+            var productList = _context.Products.ToList();
+            return View(productList);
+        }
+
+        public async Task<IActionResult> Update(int Id)
+        {
+            var product = await _context.Products.FindAsync(Id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EFCore.MVC/DAL/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿
+using EFCore.MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.MVC.DAL
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products{ get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //FluentAPI tanımlamaları
+
+            //IsRowWersion methodu ilgili property alanının EfCore tarafından otomatik şekilde güncelleneceğini belirtir.
+            //Böylece aynı kayıt üzerinde herhangi T zamanlarda farklı kişiler tarafından güncelleme geçilmeye çalışılırsa hata fırlatacaktır
+            modelBuilder.Entity<Product>().Property(x => x.RowVersion).IsRowVersion();
+            modelBuilder.Entity<Product>().Property(x => x.Price).HasPrecision(18, 2);
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
